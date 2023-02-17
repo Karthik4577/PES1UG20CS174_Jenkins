25bd10fc53bd615914334c7d2d61a27fe935d622
Gnana Karthik Jampani <89727438+Karthik4577@users.noreply.github.com>
2023-02-17 14:46:31 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,7 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "Hello,PES1UG20CS174\n";
+  cout << "Hello,this is Karthik";
   cout << "I have successfully built and run\n";
   return 0;
 }
